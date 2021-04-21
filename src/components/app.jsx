a80efda9b40be26905c9d9a1373136d376b88948
nagarajankola <ankola.nagraj07@gmail.com>
2021-04-21 15:25:09 +0530

    added keys to notes

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,27 +1,21 @@
 import React from "react";
-import Header from "./header"
-import Note from "./note"
-import Footer from "./footer"
-import notes from "../notes"
+import Header from "./header";
+import Note from "./note";
+import Footer from "./footer";
+import notes from "../notes";
 
-
-function createNotes(noteItem){
-        return(
-        <Note 
-                title = {noteItem.title}
-                content = {noteItem.content}
-            />
-        );
+function createNotes(noteItem) {
+  return <Note key={noteItem.key} title={noteItem.title} content={noteItem.content} />;
 }
 
-function App(){
-    return (
-        <div>
-            <Header />
-            {notes.map(createNotes)}
-            <Footer /> 
-        </div>
-    );
+function App() {
+  return (
+    <div>
+      <Header />
+       {notes.map(createNotes)} 
+       <Footer />
+    </div>
+  );
 }
 
 export default App;
